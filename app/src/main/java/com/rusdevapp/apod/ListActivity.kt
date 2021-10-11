c429d196d32f348a35fdaae1370d6f7cbc6d035c
Kirill1995-x <70485074+Kirill1995-x@users.noreply.github.com>
2021-10-11 22:18:03 +0300

    1. Configured work with spinners and added spinners in onSaveInstanceState and onRestoreInstanceState

--- app/src/main/java/com/rusdevapp/apod/ListActivity.kt
+++ app/src/main/java/com/rusdevapp/apod/ListActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.GridLayout
 import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
@@ -29,16 +30,43 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityListBinding.inflate(layoutInflater)
         setContentView(binding.root)
         //---
+        val startDayArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_day))
+        startDayArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spStartDay.adapter=startDayArrayAdapter
+        val startMonthArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_month))
+        startMonthArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spStartMonth.adapter=startMonthArrayAdapter
+        val startYearArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_year))
+        startYearArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spStartYear.adapter=startYearArrayAdapter
+        val endDayArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_day))
+        endDayArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spEndDay.adapter=endDayArrayAdapter
+        val endMonthArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_month))
+        endMonthArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spEndMonth.adapter=endMonthArrayAdapter
+        val endYearArrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this,
+                               R.layout.item_spinner, resources.getStringArray(R.array.array_year))
+        endYearArrayAdapter.setDropDownViewResource(R.layout.item_spinner_dropdown)
+        binding.spEndYear.adapter=endYearArrayAdapter
+        //---
         if(resources.configuration.orientation==Configuration.ORIENTATION_PORTRAIT)
             binding.rvList.layoutManager = LinearLayoutManager(this)
         else
             binding.rvList.layoutManager = GridLayoutManager(this, 2)
         //---
-        if(savedInstanceState!=null)
+        if(savedInstanceState?.getParcelableArrayList<APODModel>("arrayList") != null)
         {
-            listOfAPOD = savedInstanceState.getParcelableArrayList<APODModel>("arrayList")!!
+            listOfAPOD = savedInstanceState.getParcelableArrayList<APODModel>("arrayList")
+                         as ArrayList<APODModel>
             binding.rvList.adapter = APODAdapter(listOfAPOD, this)
         }
+        binding.btnShowList.setOnClickListener(this)
     }
 
     override fun onClick(v: View) {
@@ -57,9 +85,12 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
         val end_month:String = binding.spEndMonth.selectedItem.toString()
         val end_year:String = binding.spEndYear.selectedItem.toString()
         //---
-        if(start_day == R.string.day.toString() || end_day == R.string.day.toString() ||
-           start_month == R.string.month.toString() || end_month == R.string.month.toString() ||
-           start_year == R.string.year.toString() || end_year == R.string.year.toString())
+        if(start_day == resources.getString(R.string.day) ||
+           end_day == resources.getString(R.string.day) ||
+           start_month == resources.getString(R.string.month) ||
+           end_month == resources.getString(R.string.month) ||
+           start_year == resources.getString(R.string.year) ||
+           end_year == resources.getString(R.string.year))
             DialogError().show(supportFragmentManager, "dialog")
        else
            getListAPOD("$start_year-$start_month-$start_day", "$end_year-$end_month-$end_day")
@@ -101,6 +132,22 @@ class ListActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putParcelableArrayList("arrayList", listOfAPOD)
+        if(::listOfAPOD.isInitialized)outState.putParcelableArrayList("arrayList", listOfAPOD)
+        outState.putInt("start_day", binding.spStartDay.selectedItemPosition)
+        outState.putInt("start_month", binding.spStartMonth.selectedItemPosition)
+        outState.putInt("start_year", binding.spStartYear.selectedItemPosition)
+        outState.putInt("end_day", binding.spEndDay.selectedItemPosition)
+        outState.putInt("end_month", binding.spEndMonth.selectedItemPosition)
+        outState.putInt("end_year", binding.spEndYear.selectedItemPosition)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        binding.spStartDay.setSelection(savedInstanceState.getInt("start_day"))
+        binding.spStartMonth.setSelection(savedInstanceState.getInt("start_month"))
+        binding.spStartYear.setSelection(savedInstanceState.getInt("start_year"))
+        binding.spEndDay.setSelection(savedInstanceState.getInt("end_day"))
+        binding.spEndMonth.setSelection(savedInstanceState.getInt("end_month"))
+        binding.spEndYear.setSelection(savedInstanceState.getInt("end_year"))
     }
 }
